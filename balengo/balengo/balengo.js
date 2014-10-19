3cb63fe18e196779f2863f1f20e7df7f0467926c
jagni <jagnidasa@gmail.com>
2014-10-19 11:04:50 -0300

    Adição de alvo

--- balengo/balengo/balengo.js
+++ balengo/balengo/balengo.js
@@ -13,17 +13,21 @@ var t = 0;
 
 //Sprites
 var balengotengo = new createjs.Bitmap("balengotengo.png");
+var alvo = new createjs.Bitmap("alvo.png");
 var menino = criaMenino();
 
-
-function criaMenino(){
-	var data = {
+function criaMenino()
+{
+	var data = 
+	{
 		framerate: 10,
 		images: ["menino.jpg"],
-		frames: {
+		frames: 
+		{
 			width:104, height:100
 		},
-		animations: {
+		animations:
+		{
 			idle:0,
 			joga:1
 		}
@@ -34,7 +38,8 @@ function criaMenino(){
 	return animation;
 }
 
-function init() {
+function init()
+{
 	var canvas = document.getElementById("canvas");
 	stage = new createjs.Stage(canvas);
 	
@@ -48,7 +53,11 @@ function init() {
 	balengotengo.regY = 10; 
 	
 	menino.regX = menino.regY = 50;
+	
+	alvo.x = 300;
+	alvo.y = 100;
 
+	stage.addChild(alvo);
 	stage.addChild(menino);
 	stage.addChild(balengotengo);
 	
@@ -58,7 +67,8 @@ function init() {
 
 function lancar()
 {
-	if(!lancou){
+	if(!lancou)
+	{
 		//Cálculo da força inicial, distribuída de acordo com o angulo
 		velocidadeInicialX = forca * Math.cos(angle * Math.PI/180);
 		velocidadeInicialY =  forca *  Math.sin(-angle * Math.PI/180);
@@ -73,24 +83,37 @@ function tick(event) {
 	angle = Math.atan2(stage.mouseY -menino.y, stage.mouseX -menino.x ); 
 	angle = angle * (180/Math.PI);
 	
-	if(angle>-90 && angle<=0){ //Limitação dos angulos que o menino olha
+	if(angle>-90 && angle<=0) //Limitação dos angulos que o menino olha
+	{ 
 		menino.rotation = angle;
-		}
+	}
 	
-	if(lancou){ //Processo de lançamento
+	if(lancou) //Processo de lançamento
+	{ 
 		//Fórmulas do lançamento oblíquo
 		balengotengo.y = menino.y - velocidadeInicialY*t - (gravidade*t*t)/2;
 		balengotengo.x = menino.x + velocidadeInicialX*t;
 		
 		balengotengo.rotation+=20; //Apenas para o projétil girar
 		t+=0.5;
-		}
-		
-	if(balengotengo.x < 0 || balengotengo.x > 800 || balengotengo.y>600 || balengotengo.y<0){
-		//Caso o projétil saia da tela, se encerra o lançamento atual
+	}
+	
+	//Caso o projétil saia da tela, se encerra o lançamento atual
+	if(balengotengo.x < 0 || balengotengo.x > 800 || balengotengo.y>600 || balengotengo.y<0)
+	{
 		menino.gotoAndPlay("idle");
 		lancou = false;
-		}
+	}
+	
+	//Checagem de colisão
+	alvo.alpha = 0.2;
+	var ponto = balengotengo.localToLocal(0,0,alvo); //Posição do projétil relativo ao alvo
+	
+	//Checagem se este ponto está por cima de algum pixel do alvo
+	if (alvo.hitTest(ponto.x, ponto.y)) 
+	{
+		alvo.alpha = 1; 
+	}
 		
 	stage.update();
 }
