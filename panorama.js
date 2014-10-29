237768d98d6023011c398064af884fd322fafdc1
jagni <jagnidasa@gmail.com>
2014-10-29 00:07:57 -0300

    Teste de balões - Pop-ups

--- panorama.js
+++ panorama.js
@@ -3,11 +3,7 @@ var stage;
 var dragContainer = new createjs.Container(); //Cenário como todo
 var secao1 = new createjs.Container(); //Praça
 var secao2 = new createjs.Container(); //Lagoa
-var bitmap = new createjs.Bitmap("cenarios/cenario1.png");
-var maxPositionX;
-//Setas
-var right ; 
-var left ;
+var balao = new createjs.Container();
 
  //Booleanos de controle da camera
 var moveLeft;
@@ -19,13 +15,11 @@ function init() {
 	stage.enableMouseOver(20);  
 	stage.addChild(dragContainer);
 
-	
-
-	
+	var bitmap = new createjs.Bitmap("cenarios/cenario1.png");
 	//var bitmap = new createjs.Bitmap("cenarios/teste.jpg"); //Containers não possuem width e height definido, por isso estou pegando os da imagem
 	secao1.width = bitmap.image.width;
 	secao1.height = bitmap.image.height;
-	maxPositionX = - (secao1.width - stage.canvas.width);
+	var maxPositionX = - (secao1.width - stage.canvas.width);
 	dragContainer.maxPositionX = maxPositionX; //Limites de posicionamento dele
 	//dragContainer.y = dragContainer.maxPositionY; //Só para começar no canto esquerdo de baixo
 	
@@ -49,10 +43,12 @@ function init() {
 		var lVideoDOMElement = new createjs.DOMElement(lVideo);
 		lVideoDOMElement.x = -100;
 		lVideoDOMElement.y = 10;*/
+		//secao1.addChild(lVideoDOMElement);
 	
 	secao1.addChild(bitmap);
-	//secao1.addChild(lVideoDOMElement);
 	secao1.addChild(criaRelogio());
+	secao1.addChild(criaBela());
+	
 	var olha = new createjs.Bitmap("sprites/olha.png");
 	olha.x = 600;
 	olha.y = 300;
@@ -63,8 +59,6 @@ function init() {
 	secao2.height = bitmap2.image.height;
 	secao2.x = secao1.width;
 	secao2.addChild(bitmap2);
-	secao2.addChild(criaBela());
-	
 	
 
 	var rectRight = new createjs.Shape(new createjs.Graphics().beginFill("#00000").drawRect(stage.canvas.width*0.90, 0, stage.canvas.width*0.10, stage.canvas.height));
@@ -94,8 +88,9 @@ function init() {
 	left.x = 10;
 	left.y = stage.canvas.height /2 - 50;
 	
-	dragContainer.addChild(secao1);
+	
 	dragContainer.addChild(secao2);
+	dragContainer.addChild(secao1);
 
 	stage.addChild(left);
 	stage.addChild(right);
@@ -111,9 +106,9 @@ function clickR(event){
 	var positionX = dragContainer.x - stage.canvas.width;
 	moveRight = false;
 	if(positionX >= dragContainer.maxPositionX)
-		createjs.Tween.get(dragContainer).to({x:positionX},2000, createjs.Ease.getElasticOut(1, 2));
+		createjs.Tween.get(dragContainer, {override : true}).to({x:positionX},2000, createjs.Ease.getElasticOut(1, 2));
 	else
-		createjs.Tween.get(dragContainer).to({ x : dragContainer.maxPositionX } , 2000, createjs.Ease.getElasticOut(1, 2));
+		createjs.Tween.get(dragContainer, {override : true}).to({ x : dragContainer.maxPositionX } , 2000, createjs.Ease.getElasticOut(1, 2));
 }
 
 function overL(event){
@@ -125,9 +120,9 @@ function clickL(event){
 	moveLeft = false;
 	
 	if(positionX <= 0)
-		createjs.Tween.get(dragContainer).to({x:positionX,visible:true},2000, createjs.Ease.getElasticOut(1, 2));
+		createjs.Tween.get(dragContainer, {override : true}).to({x:positionX,visible:true},2000, createjs.Ease.getElasticOut(1, 2));
 	else
-		createjs.Tween.get(dragContainer).to({ x : 0} , 2500, createjs.Ease.getElasticOut(1, 2));
+		createjs.Tween.get(dragContainer, {override : true}).to({ x : 0} , 2500, createjs.Ease.getElasticOut(1, 2));
 }
 
 
@@ -147,7 +142,7 @@ function criaRelogio(){
 
 	var spriteSheet = new createjs.SpriteSheet(data);
 	var animation = new createjs.Sprite(spriteSheet, "idle");
-	animation.x = secao1.width/2;
+	animation.x = 800;
 	animation.y = 0; //secao1.height/2;
 	
 	if (!createjs.Sound.initializeDefaultPlugins()) {return;}
@@ -156,6 +151,9 @@ function criaRelogio(){
     var manifest = [
         {id:"poim", src:"poim.mp3"},
     ];
+	
+	animation.id = "Relógio";
+	animation.texto= "tick tack";
  
     createjs.Sound.alternateExtensions = ["mp3"];
     createjs.Sound.registerManifest(manifest, audioPath);
@@ -167,6 +165,9 @@ function criaRelogio(){
 			clicado = true;
 			dragContainer.maxPositionX -=  secao2.width;
 			}
+			
+		if(balao.balaoAtivo != animation.id)
+			criaBalao(animation);
 	});
 	
 	return animation;
@@ -187,8 +188,8 @@ function criaBela(){
 	
 	var spriteSheet = new createjs.SpriteSheet(data);
 	var animation = new createjs.Sprite(spriteSheet, "idle");
-	animation.x = 2400;
-	animation.y = secao2.height/2;
+	animation.x = 500;
+	animation.y = 300;
 
 	if (!createjs.Sound.initializeDefaultPlugins()) {return;}
  
@@ -200,12 +201,53 @@ function criaBela(){
     createjs.Sound.alternateExtensions = ["mp3"];
     createjs.Sound.registerManifest(manifest, audioPath);
 	
+	animation.id = "Bela";
+	animation.texto= "teste";
+	
 	animation.on("click", function move(evt){
 		animation.gotoAndPlay("run");
 		createjs.Sound.play("magia");
+		if(balao.balaoAtivo != animation.id)
+			criaBalao(animation);
 	});
 	
 	return animation;
+}
+
+function criaBalao(animationAlvo){
+	balao.removeAllChildren();
+
+	var shapeBalao = new createjs.Shape(new createjs.Graphics().beginFill("#00000").drawRoundRect( 0, 0, 200, 100, 5 ));
+	var texto = new createjs.Text(animationAlvo.texto, "20px Arial", "#FFFFFF");
+	balao.addChild(shapeBalao);
+	balao.addChild(texto);
+	
+	balao.balaoAtivo = animationAlvo.id;
+	
+	balao.regX = 0;
+	balao.regY = 100;
+	balao.x = animationAlvo.x + animationAlvo.spriteSheet.getFrameBounds(0).width;
+	balao.y = animationAlvo.y;
+	
+	console.log(balao.isVisible());
+	
+	if(balao.y <= 10)
+	{
+		balao.regY = 0;
+		balao.y = animationAlvo.y + animationAlvo.spriteSheet.getFrameBounds(0).height;
+	}
+	
+	if(balao.x + 200 > -dragContainer.x + 800)
+	{
+		balao.regX = 200;
+		balao.x = animationAlvo.x;
+	}
+		
+	animationAlvo.parent.addChild(balao);
+	
+	balao.scaleX = 0;
+	balao.scaleY = 0;
+	createjs.Tween.get(balao).to({scaleX:1, scaleY:1, visible:true},500, createjs.Ease.getElasticInOut(6, 2));
 }
 
 
