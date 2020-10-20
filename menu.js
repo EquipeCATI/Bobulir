5c267dfcd05f29893fc07f438f94b1321f8c41ae
Jagni Dasa <jagnidasa@gmail.com>
2020-10-20 00:18:55 -0300

    Reestruturação de arquivos para Github Pages

---
+++ menu.js
@@ -0,0 +1,326 @@
+var stage;
+var containerMenu = new createjs.Container();
+var containerFundo = new createjs.Container();
+var containerCreditos = new createjs.Container();
+var bitmapStart;
+var bitmapCredit;
+var preloadMenu = new createjs.LoadQueue(false);
+var pipa;
+var emFlor;
+var bobulir;
+var botaoVoltar = criaBotaoVoltar();
+var credito;
+//var pipa = spritePipa("assets/sprites/spritePipa.png");
+
+
+function carregaAssets(){
+
+	var canvas = document.getElementById("canvas");
+	stage = new createjs.Stage(canvas);
+
+	stage.enableMouseOver(20);  
+	createjs.Ticker.on("tick", tick);
+
+	preloadMenu.on("complete", handleCompleteMenu);
+
+	var manifest = [
+		{src:"sprites/spritePipa.png", id:"pipa"},
+		{src:"images/icons/iniciar.png", id:"iniciar"},
+		{src:"images/icons/creditos.png", id:"creditos"},
+		{src:"images/nuv/nuvemPequena.png", id:"nuvem1"},
+		{src:"images/nuv/nuvemMedia.png", id:"nuvem2"},
+		{src:"images/nuv/nuvemGrande.png", id:"nuvem3"},
+		{src:"images/bobulir/bobulir.png", id:"bobulir"},
+		{src:"images/bobulir/bobulirDanca.png", id:"danca"},
+		{src:"images/bobulir/bobulirPoim.png", id:"poim"},
+		{src:"creditos.png", id:"credito"},
+		];
+	preloadMenu.loadManifest(manifest, true, "assets/");
+	var audioPath = "assets/audio/";
+	var manifestAudio = [
+		{id:"emflor", src:"emflor.m4a"},
+		{id:"vibraSlap", src : "vibraSlap.wav"}
+	];
+	createjs.Sound.alternateExtensions = ["mp3"];
+	createjs.Sound.registerManifest(manifestAudio, audioPath);
+	}
+	
+function stop() {
+	if (preloadMenu != null) { preloadMenu.close(); }
+	}
+	
+function handleCompleteMenu(event) {
+	menu();
+}
+
+function criaIniciar(){
+	var data = {
+		framerate: 10, //Velocidade de troca de frame - irrelevante
+		images: [preloadMenu.getResult("iniciar")], //spriteSheet
+		frames: {
+			width:300, height:120// tamanho dos frames
+		},
+		animations: { //Associação de nomes de animação aos frames
+			idle: [0],
+			over : [0, 29, true],
+			normalize : [0],
+		}
+	};
+
+	var spriteSheet = new createjs.SpriteSheet(data);
+	var botao = new createjs.Sprite(spriteSheet, "idle");
+	botao.regX = 150;
+	botao.regY = 60;
+	return botao;
+}
+
+function criaCreditos(){
+	var data = {
+		framerate: 10, //Velocidade de troca de frame - irrelevante
+		images: [preloadMenu.getResult('creditos')], //spriteSheet
+		frames: {
+			width:195, height:183// tamanho dos frames
+		},
+		animations: { //Associação de nomes de animação aos frames
+			idle: [0],
+			over: [1],
+			down: [0, 4, false],
+			up: [4, 3, 2, 1, 0],
+		}
+	};
+
+	var spriteSheet = new createjs.SpriteSheet(data);
+	var botao = new createjs.Sprite(spriteSheet, "idle");
+	botao.scaleX = botao.scaleY = 0.25;
+	return botao;
+}
+
+function menu(){
+    var fundo = new createjs.Shape(new createjs.Graphics().beginFill("#6fc5ce").drawRect(0, 0, 1600, 600));
+	containerFundo.addChild(fundo);
+	for(var i = 0; i <= 5; i++){
+			criaNuvensEsq();
+			criaNuvensDir();
+	}
+	
+	containerMenu.addChild(containerFundo);
+	credito = new createjs.Bitmap(preloadMenu.getResult("credito"));
+	credito.y = 600;
+	credito.regX = credito.image.width/2;
+	credito.x = 1200;
+	containerMenu.addChild(credito);
+	//criação do Botão start.
+	//bitmapStart = new createjs.Bitmap("startBtn.png");
+	bitmapStart = criaIniciar();//spriteBotao(preloadMenu.getResult('iniciar'));
+	bitmapCredit = criaCreditos();
+
+	bitmapStart.x = 400;
+	bitmapStart.y = 500;
+	containerMenu.addChild(bitmapStart);
+
+	bitmapStart.on("click", btnClicked);
+	bitmapStart.on("mouseover", over);
+	bitmapStart.on("mouseout", out);
+	//botao dos creditos
+
+	bitmapCredit.x = 800 - 48.75 - 10;
+	bitmapCredit.y = 600 - 45.75 - 10;
+	containerMenu.addChild(bitmapCredit);
+
+	pipa = spritePipa(preloadMenu.getResult('pipa'));
+	pipa.x = 300;
+	pipa.y = 50;
+	containerMenu.addChild(pipa);
+	
+	bobulir = criaBobulir();
+	bobulir.regX = bobulir.width/2;
+	bobulir.regY = bobulir.height/2;
+	//bobulir.scaleX = bobulir.scaleY = 0.25;
+	bobulir.x = 400;
+	bobulir.y = 250;
+	containerMenu.addChild(bobulir);
+
+	var nome = new createjs.Bitmap(preloadMenu.getResult("bobulir"));
+	nome.regX = nome.image.width/2;
+	nome.regY = nome.image.height/2;
+	//bobulir.scaleX = bobulir.scaleY = 0.25;
+	nome.x = 399;
+	nome.y = 267;
+	containerMenu.addChild(nome);
+	stage.addChild(containerMenu);
+	emFlor = createjs.Sound.play("emflor");
+	emFlor.volume = 0.2;
+
+	//adicionando o botao dos Creditos para voltar.
+	containerMenu.addChild(botaoVoltar);
+
+	bitmapCredit.on("click", btnClicked);
+	bitmapCredit.on("mouseover", over);
+	bitmapCredit.on("mouseout", out);
+	bitmapCredit.addEventListener("mousedown", down);
+}
+
+function criaBobulir(){
+	var data = {
+		framerate: 10, //Velocidade de troca de frame - irrelevante
+		images: [preloadMenu.getResult("danca"), preloadMenu.getResult("poim")], //spriteSheet
+		frames: {
+			width:590, height:226// tamanho dos frames
+		},
+		animations: { //Associação de nomes de animação aos frames
+			danca : [0, 59],
+			poim : [60, 82, false],
+		}
+	};
+
+	var spriteSheet = new createjs.SpriteSheet(data);
+	var bob = new createjs.Sprite(spriteSheet, "danca");
+	bob.width = 590;
+	bob.height = 226;
+	return bob;
+}
+
+function criaNuvensEsq(){
+		var nNuvem = parseInt(Math.random()*3 +1);
+		var img = preloadMenu.getResult("nuvem" + nNuvem);
+		var nuvem = new createjs.Bitmap(img);
+		nuvem.x = 100 + Math.random()*800;
+		nuvem.x = -nuvem.x;
+		nuvem.y = 10 + Math.random()*200;
+		nuvem.scaleX = nuvem.scaleY = 0.5;
+		createjs.Tween.get(nuvem, {override : true}).to({ x : 1600} , 60000 + Math.random()*25000).call(criaNuvensDir);
+		containerFundo.addChild(nuvem);
+}
+function criaNuvensDir(){
+		var nNuvem = parseInt(Math.random()*3 +1);
+		var img = preloadMenu.getResult("nuvem" + nNuvem);
+		var nuvem = new createjs.Bitmap(img);
+		nuvem.x = 100 + Math.random()*800;
+		nuvem.x = 1600 + nuvem.x;
+		nuvem.y = 100 + Math.random()*200;
+		nuvem.scaleY = 0.5;
+		nuvem.scaleX = -0.5;
+		createjs.Tween.get(nuvem, {override : true}).to({ x : -200} , 60000 + Math.random()*25000).call(criaNuvensEsq);
+
+		containerFundo.addChild(nuvem);
+}
+
+function btnClicked(event){
+	if(event.target == bitmapStart){
+		createjs.Tween.get(emFlor).to({volume : 0}, 900);
+		var vibraSlap = createjs.Sound.play("vibraSlap");
+		vibraSlap.volume = 0.3;
+		createjs.Tween.get(vibraSlap).to({volume : 0}, 1000);
+		bobulir.gotoAndPlay("poim")
+		event.target.gotoAndPlay("normalize");
+		event.on("mouseup", up);
+		teste();
+	}
+	if(event.target == bitmapCredit){
+		creditos();
+	}
+
+}
+
+function teste(){
+	createjs.Tween.get(pipa, {override : true}).to({ x : 800} , 1000 ).call(carregaAssetsPanorama);
+}
+
+//Sprite de botão - usada em ambos
+function spriteBotao(caminho){
+	//Configuração do spriteSheet do botao
+	var data = {
+		framerate: 10, //Velocidade de troca de frame - irrelevante
+		images: [caminho], //spriteSheet
+		frames: {
+			width:150, height:58// tamanho dos frames
+		},
+		animations: { //Associação de nomes de animação aos frames
+			normal : 0, 
+			over : 2,
+			click : 1
+		}
+	};
+
+	var spriteSheet = new createjs.SpriteSheet(data);
+	var botao = new createjs.Sprite(spriteSheet, "normal");
+
+	
+	return botao;
+}
+
+function spritePipa(caminho){
+	//Configuração do spriteSheet do botao
+	var data = {
+		framerate: 24, //Velocidade de troca de frame - irrelevante
+		images: [caminho], //spriteSheet
+		frames: {
+			width:650, height:420 // tamanho dos frames
+		},
+		animations: { //Associação de nomes de animação aos frames
+			loop : [0, 79]
+			//flyOut :[ ]
+		}
+	};
+
+	var spriteSheet = new createjs.SpriteSheet(data);
+	var animacao = new createjs.Sprite(spriteSheet, "loop");
+	animacao.speed = 2;
+	animacao.scaleX = 0.4;
+	animacao.scaleY = 0.4;
+	return animacao;
+}
+
+
+
+function over(event){
+	event.target.gotoAndPlay("over");
+}
+function out(event){
+	event.target.gotoAndPlay("idle");
+}
+function down(event){
+	event.target.gotoAndPlay("down");
+	event.on("mouseup", up);
+}
+function up(event){
+	event.target.gotoAndPlay("idle");
+}
+
+function tick(){
+	stage.update();
+}
+
+ function creditos(){
+ 	createjs.Tween.get(containerMenu).to({ x : -800} ,500).call(loopCred);
+
+ }
+
+ function loopCred(){
+ 	createjs.Tween.get(credito).to({ y : -credito.image.height} ,30000).to({ y : 600} , 1).call(loopCred);
+ }
+ 
+ function criaBotaoVoltar(){
+	var botao = new createjs.Container();
+	
+	var shapeBotao = new createjs.Shape(new createjs.Graphics().beginFill("#ed682c").drawRoundRect( 0, 0, 100, 50, 5 ));
+	botao.addChild(shapeBotao);
+	
+	var titulo = new createjs.Text("Voltar", "50px Bahiana", "#ffffff");
+	var b = titulo.getBounds();
+	titulo.x = 50 - b.width/2;
+	botao.addChild(titulo);
+	
+	botao.x = 820;
+	botao.y = 530;
+	
+	botao.on("click", function(evt){
+		createjs.Tween.get(containerMenu).to({ x : 0} , 500);
+		createjs.Tween.removeTweens(credito);
+		credito.y = 600;
+	});
+	
+	return botao;
+	//titulo.y = 15;
+	
+}
---
+++ zerimOuUm/zerimOuUm.js
@@ -0,0 +1,257 @@
+var containerZerim = new createjs.Container();
+var preloadZerim = new createjs.LoadQueue(false);
+
+var jogador;
+var jogador1;
+var jogador2;
+var jogador3;
+
+var botao0;
+var botao1;
+
+var animacaoZerim;
+
+
+function carregaAssetsZerim(){
+	preloadZerim.on("complete", handleCompleteZerim);
+
+	var manifest = [
+		{src:"spriteZerim.png", id:"sprite"},
+		{src:"iei.png", id:"iei"},
+		{src:"back.png", id:"back"},
+		{src:"jogador.png", id:"baixo"},
+		{src:"jogadorCima.png", id:"cima"},
+		{src:"jogadorEsq.png", id:"esquerda"},
+		{src:"jogadorDir.png", id:"direita"},
+		{src:"0ou1.png", id:"0ou1"},
+		];
+	preloadZerim.loadManifest(manifest, true, "zerimOuUm/");
+
+	if (!createjs.Sound.initializeDefaultPlugins()) {return;}
+ 
+	var audioPath = "zerimOuUm/";
+	var manifestAudio = [
+		{id:"ieei", src:"ieei.wav"},
+		{id:"zerimOUm", src:"zerimOUm.wav"},
+	];
+	createjs.Sound.alternateExtensions = ["mp3"];
+	createjs.Sound.registerManifest(manifestAudio, audioPath);
+}
+	
+function stop() {
+	if (preloadZerim != null) { preloadZerim.close(); }
+	}
+	
+function handleCompleteZerim(event) {
+	//zerim();
+}
+
+function criaJogador(id){
+	//Configuração do spriteSheet do botao
+	var image = new createjs.Bitmap(preloadZerim.getResult(id));
+	var data = {
+		framerate: 10, //Velocidade de troca de frame - irrelevante
+		images: [preloadZerim.getResult(id)], //spriteSheet
+		frames: {
+			width:image.image.width/2, height:image.image.height// tamanho dos frames
+		},
+		animations: { //Associação de nomes de animação aos frames
+			zero : [0], 
+			um : [1],
+		}
+	};
+	var spriteSheet = new createjs.SpriteSheet(data);
+	var mao = new createjs.Sprite(spriteSheet, "zero");
+	mao.regX = image.image.width/4;
+	mao.regY = image.image.height/2;
+	return mao;
+}
+
+function cria0ou1(){
+	var data = {
+		framerate: 10, //Velocidade de troca de frame - irrelevante
+		images: [preloadZerim.getResult("0ou1")], //spriteSheet
+		frames: {
+			width:800, height:600// tamanho dos frames
+		},
+		animations: { //Associação de nomes de animação aos frames
+			idle : [0], 
+			anima : [0, 34, false],
+		}
+	};
+	var spriteSheet = new createjs.SpriteSheet(data);
+	var animation = new createjs.Sprite(spriteSheet, "idle");
+	return animation;
+}
+
+function getZerim(){	
+	containerZerim = new createjs.Container();
+	animacaoZerim = cria0ou1();
+	botao0 = criaMaoZerim();
+	botao1 = criaMaoZerim();
+	jogador  = criaJogador("baixo");
+	jogador.x = 400;
+	jogador.y = 600 - jogador.regY/2;
+	jogador1 = criaJogador("esquerda");
+	jogador1.x = jogador.regX/2;
+	jogador1.y = 300;
+
+	jogador2 = criaJogador("cima");
+	jogador2.x = 400;
+	jogador2.y = jogador2.regX/2; 
+
+	jogador3 = criaJogador("direita");
+	jogador3.x = 800 - jogador3.regX/2;
+	jogador3.y = 300; 
+	
+	containerZerim = new createjs.Container();
+	
+	//Fundo
+	var fundo = new createjs.Bitmap(preloadZerim.getResult("back"));
+	containerZerim.addChild(fundo);
+
+	console.log("blz");
+	botao0.gotoAndPlay("zero");
+	botao0.x = 200;
+	botao0.y = 500;
+	
+	botao1.gotoAndPlay("um");
+	botao1.x = 500;
+	botao1.y = 500;
+	
+	botao0.on("click", clicou);
+	botao1.on("click", clicou);
+	
+	containerZerim.addChild(jogador);
+	containerZerim.addChild(jogador1);
+	containerZerim.addChild(jogador2);
+	containerZerim.addChild(jogador3);
+	
+	containerZerim.addChild(botao0);
+	containerZerim.addChild(botao1);
+
+	criaTutorialZerim();
+}
+
+function clicou(evt){
+	jogador.alpha = 0;
+	jogador1.alpha = 0;
+	jogador2.alpha = 0;
+	jogador3.alpha = 0;
+	containerZerim.addChild(animacaoZerim);
+	createjs.Sound.play("zerimOUm");
+	if (evt.target == botao0){
+		jogador.gotoAndPlay("zero");
+		jogador1.gotoAndPlay("um");
+		jogador2.gotoAndPlay("um");
+		jogador3.gotoAndPlay("um");
+	}
+	else{
+		jogador.gotoAndPlay("um");
+		jogador1.gotoAndPlay("zero");
+		jogador2.gotoAndPlay("zero");
+		jogador3.gotoAndPlay("zero");
+	}
+	animacaoZerim.gotoAndPlay("anima");
+	containerZerim.on("tick", exibeZerim);
+}
+
+function exibeZerim(){
+	if(animacaoZerim.currentFrame == 34){
+		animacaoZerim.alpha = 0;
+		animacaoZerim.gotoAndPlay("idle");
+		jogador.alpha = 1;
+		jogador1.alpha = 1;
+		jogador2.alpha = 1;
+		jogador3.alpha = 1;
+		var iei = new createjs.Bitmap(preloadZerim.getResult("iei"));
+		createjs.Sound.play("ieei");
+		iei.scaleX = iei.scaleY = 0;
+		iei.regX = iei.image.width/2;
+		iei.regY = iei.image.height/2;
+		
+		iei.x = 400;
+		iei.y = 300;
+		containerZerim.addChild(iei);
+		createjs.Tween.get(iei,  {override : true}).to({ scaleX : 0.75, scaleY : 0.75} , 1000).to({ scaleX : 0.5, scaleY : 0.5} , 1000).call(terminou);
+	}
+}
+
+var containerTutorialZerim;
+function criaTutorialZerim(){
+	containerTutorialZerim = new createjs.Container();
+	var fundoTuto = new createjs.Shape(new createjs.Graphics().beginFill("#000000").drawRoundRect(0, 0, stage.canvas.width, stage.canvas.height, 10));
+	fundoTuto.alpha = 0.5;
+	containerTutorialZerim.addChild(fundoTuto);
+	
+	var balaoTuto = new createjs.Container();
+	
+	var shapeBalao = new createjs.Shape(new createjs.Graphics().beginFill("#6fc5ce").drawRoundRect( 0, 0, 400, 300, 5 ));
+	balaoTuto.x = 200;
+	balaoTuto.y = 200;
+	balaoTuto.addChild(shapeBalao);
+	
+	var texto = new createjs.Text("Eita, a raia caiu na Lagoa do Urubu! A galera tá morrendo de medo de ir lá por causa da Isaura! Bora tirar zerim ou um e ver quem vai?", "20px FiraSans", "#000000");
+	texto.x = 15;
+	texto.y = 75;
+	texto.lineWidth = 370;
+	texto.lineHeight = 25;
+	balaoTuto.addChild(texto);
+	
+	balaoTuto.addChild(criaBotaoTutoZerim());
+	containerTutorialZerim.addChild(balaoTuto);
+	containerZerim.addChild(containerTutorialZerim);
+	containerTutorialZerim.regX = 400;
+	containerTutorialZerim.regY = 300;
+	containerTutorialZerim.x = 400;
+	containerTutorialZerim.y = 300;
+}
+
+function criaBotaoTutoZerim(){
+	var botao = new createjs.Container();
+	
+	var shapeBotao = new createjs.Shape(new createjs.Graphics().beginFill("#ed682c").drawRoundRect( 0, 0, 180, 50, 5 ));
+	botao.addChild(shapeBotao);
+	
+	var titulo = new createjs.Text("É o jeito...", "50px Bahiana", "#ffffff");
+	var b = titulo.getBounds();
+	titulo.x = 90 - b.width/2;
+	titulo.y = 5;
+	botao.addChild(titulo);
+	
+	botao.x = 110;
+	botao.y = 200;
+	
+	botao.on("click", function(evt){
+		createjs.Tween.get(containerTutorialZerim, {override : true}).to({ scaleX : 0, scaleY : 0, status : "volta"} , 500);
+		endTutorialBalengo = true;
+	});
+	
+	return botao;
+}
+
+function terminou(){
+	containerZerim.parent.removeChild(containerZerim);
+	panoramaIsActive = true;
+}
+
+function criaMaoZerim(){
+	//Configuração do spriteSheet do botao
+	var data = {
+		framerate: 10, //Velocidade de troca de frame - irrelevante
+		images: [preloadZerim.getResult("sprite")], //spriteSheet
+		frames: {
+			width:100, height:100// tamanho dos frames
+		},
+		animations: { //Associação de nomes de animação aos frames
+			zero : 0, 
+			um : 1,
+			idle : 2
+		}
+	};
+	var spriteSheet = new createjs.SpriteSheet(data);
+	var mao = new createjs.Sprite(spriteSheet, "idle");
+	mao.hitArea = new createjs.Shape(new createjs.Graphics().beginFill("#ffffff").drawRect( 0, 0, 100, 100));
+	
+	return mao;
+}
